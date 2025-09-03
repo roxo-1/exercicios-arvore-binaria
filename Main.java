f8e06cc8f45afb649dfdd6d039f8ee458ab40757
roxo-1 <carolzinhalee09@gmail.com>
2025-09-03 14:05:13 -0300

    UPDATE Main testes

--- Main.java
+++ Main.java
@@ -2,6 +2,16 @@ import java.util.Random;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println(altura((null));
+        No p = new No(50);
+        p.esq(40);
+        p.dir(60);
+        p.esq.esq(35);
+        p.esq.dir(45);
+        p.dir.esq(55);
+        p.dir.dir(65);
+        System.out.println(altura((60));
+        int nivel = 0;
+        nivelNo(65, nivel);
+        mostraArvore(No p, String espaco);
     }
 }
